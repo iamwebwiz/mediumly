0c6696dde07aae6440167ee74d4fb30950b9f0ef
Ezekiel <iamwebwiz@gmail.com>
2019-05-24 04:17:20 +0100

    Add Admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->guest() or !auth()->user()->is_admin) {
+            return redirect('/login');
+        }
+
+        return $next($request);
+    }
+}
